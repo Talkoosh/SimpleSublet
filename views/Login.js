9b32c5a5475bc4f7690456ecc7001bbfcd8c7f15
TalSB <artishok.tal@gmail.com>
2023-08-26 19:00:09 +0300

    worked on login screen layout and styling

--- views/Login.js
+++ views/Login.js
@@ -1,34 +1,40 @@
-import { StyleSheet, Text, View, Dimensions, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 
 const Login = () => {
     const { control, handleSubmit } = useForm();
 
-    const onSubmit = (data) => {};
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <View style={styles.logo}></View>
-            <Controller
-                control={control}
-                rules={{
-                    required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => <TextInput placeholder="Email" onBlur={onBlur} onChangeText={onChange} value={value} />}
-                name="email"
-            />
-            <Controller
-                control={control}
-                rules={{
-                    required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => <TextInput placeholder="Password" onBlur={onBlur} onChangeText={onChange} value={value} />}
-                name="password"
-            />
-            <Button title="submit" onPress={handleSubmit(onSubmit)}>
-                Login
-            </Button>
-        </View>
+            <View>
+                <Text>Email</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                        required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => <TextInput style={styles.input} placeholder="Email" onBlur={onBlur} onChangeText={onChange} value={value} />}
+                    name="email"
+                />
+                <Text>Password</Text>
+                <Controller
+                    control={control}
+                    rules={{
+                        required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => <TextInput style={styles.input} placeholder="Password" onBlur={onBlur} onChangeText={onChange} value={value} />}
+                    name="password"
+                />
+            </View>
+            <TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.loginButton}>
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 };
 
@@ -40,12 +46,29 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
         width: Dimensions.get('window').width,
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
     },
     logo: {
         width: 100,
         height: 100,
         backgroundColor: 'white',
+    },
+    input: {
+        width: 200,
+        height: 50,
+        backgroundColor: 'white',
+        marginBottom: 5,
+        padding: 20,
+    },
+    loginButton: {
+        backgroundColor: 'blue',
+        width: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+    },
+    loginText: {
+        color: 'white',
     },
 });
